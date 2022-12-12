17fed72034478120c621ddd5dc605cacac30b966
minkyoung kim <minry20415@naver.com>
2022-12-12 22:30:52 +0900

    [add] 힙-더맵게

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,32 +1,28 @@
 import java.util.HashSet;
 
-// 완전탐색 Lv2.소수찾기
+// Lv 2. 더 맵게
 class Solution {
-    HashSet<Integer> numberSet = new HashSet<>();
-    public void recursive(String comb, String others){
-        // 1. 현재 조합을 set에 추가
-        // Integer 형태로 바꿔줌
-        if (!comb.equals("")) // comb 맨처음에 들어왔을 땐 빈스트링이기 때문에 예외처리를 해줌
-            numberSet.add(Integer.valueOf(comb));
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
 
-        for (int i = 0; i < others.length(); i++) {
-            recursive(comb + others.charAt(i)
-                    , others.substring(0,i) + others.substring(i+1));
+        HashSet<Integer> mixScoville = new HashSet<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            if (scoville[i] < 7){
+                mixScoville.add(scoville[i]);
+            }
 
         }
-    }
-    public int solution(String numbers) {
-        int answer = 0;
-        // 1. 모든 조합의 숫자를 만든다.
-        recursive("", numbers);
-        System.out.println(numberSet);
+
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sl = new Solution();
-        String nums = "17";
-        sl.solution(nums);
 
+        int[] scoville = new int[]{1, 2, 3, 9, 10, 12};
+        int k = 7;
+
+        sl.solution(scoville, k);
     }
 }
